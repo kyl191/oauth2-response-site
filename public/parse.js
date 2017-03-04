9a1c333d029318f0f4019361a2da3fcc1ab67513
Kyle Lexmond <github@kyl191.net>
2017-03-04 15:08:54 -0500

    Add page

---
+++ public/parse.js
@@ -0,0 +1,17 @@
+"use strict";
+$(function() {
+  $("#noscript").hide();
+  var uri = new URI(window.location).search(true);
+  if (uri.hasOwnProperty("code") && uri.code &&  uri.code.length < 0){
+    $("#code").text(uri.code);
+  } else {
+    if (uri.hasOwnProperty("error")){
+      $("#error").text(uri.error);
+    }
+    if (uri.hasOwnProperty("error_description")){
+      $("#error_description").text(uri.error_description);
+    }
+    $("#failure").show();
+    $("#success").hide();
+  }
+});
